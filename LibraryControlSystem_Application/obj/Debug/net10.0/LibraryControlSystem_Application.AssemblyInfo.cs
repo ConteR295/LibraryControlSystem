357c97317a24203b5ea41780f53f0dd4d99f8956
ConteR <amirhuainov049@gmail.com>
2026-04-04 18:22:48 +0300

    ?

--- LibraryControlSystem_Application/obj/Debug/net10.0/LibraryControlSystem_Application.AssemblyInfo.cs
+++ LibraryControlSystem_Application/obj/Debug/net10.0/LibraryControlSystem_Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryControlSystem_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb045aab275333ffd17bc47e1697078cbe6fb5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7bd2d9866adb1bf4ea06c4ae65235ceecec1b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryControlSystem_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryControlSystem_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
